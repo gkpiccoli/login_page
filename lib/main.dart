eb777eb2320ff3d68464dd4d6f28dc0d872ab5d8
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-24 11:26:51 -0300

    first build

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:login_screen/pagina_login.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'página de login',
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: const PaginaLogin(),
+    );
+  }
+}
---
+++ lib/pagina_login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PaginaLogin extends StatefulWidget {
+  const PaginaLogin({super.key});
+
+  @override
+  State<PaginaLogin> createState() => _PaginaLoginState();
+}
+
+class _PaginaLoginState extends State<PaginaLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
