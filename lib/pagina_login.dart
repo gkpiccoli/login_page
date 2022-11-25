3f8634a6be05766f7dcc34dbc7702b74ab373e88
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-25 14:10:18 -0300

    tela inicial

--- lib/pagina_login.dart
+++ lib/pagina_login.dart
@@ -13,49 +13,54 @@ class _PaginaLoginState extends State<PaginaLogin> {
     return Scaffold(
       backgroundColor: Colors.deepOrangeAccent,
       body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                  autofocus: true,
-                  keyboardType: TextInputType.text,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                  ),
-                  decoration: InputDecoration(
-                      labelText: 'Nome',
-                      labelStyle: const TextStyle(color: Colors.white)),
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
                 ),
-                TextFormField(
-                  autofocus: true,
-                  keyboardType: TextInputType.number,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                  ),
-                  decoration: InputDecoration(
-                      labelText: 'CPF',
-                      labelStyle: const TextStyle(color: Colors.white)),
+                decoration: const InputDecoration(
+                    labelText: 'Nome',
+                    labelStyle: TextStyle(color: Colors.white)),
+              ),
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.number,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
                 ),
-                TextFormField(
-                  autofocus: true,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                  ),
-                  decoration: InputDecoration(
-                      labelText: 'Senha',
-                      labelStyle: const TextStyle(color: Colors.white)),
+                decoration: const InputDecoration(
+                    labelText: 'CPF',
+                    labelStyle: TextStyle(color: Colors.white)),
+              ),
+              const Divider(),
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
                 ),
-              ],
-            ),
-          )),
+                decoration: const InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(color: Colors.white)),
+              ),
+              const Divider(),
+              ButtonTheme(height: 60.0,
+              child: button,)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
